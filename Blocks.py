f72b312e0f620d485b5b39374d3e9a042510deb4
Jumpixyn <dnikych@gmail.com>
2020-02-18 18:23:43 +0500

    Added 3 more Blocks (Iron Ore, Copper Ore, JoJo Stone)

--- Blocks.py
+++ Blocks.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.rect import Rect
+from pygame import Rect
 
 
 class Block:
@@ -9,7 +9,7 @@ class Block:
         self.id = id
         self.x = x
         self.y = y
-        self.size = 10
+        self.size = 20
 
         # Имя блока, его цвет отрисовки, флаг is_breakable, прочность блока
 
@@ -39,25 +39,61 @@ class Air(Block):
         self.solidity = -1
 
 
+# Блок Кореной Породы (JJBA: Diamond Is Unbreakable)
+
+class JoJoStone(Block):
+    def __init__(self, x, y):
+        super().__init__(1, x, y)
+
+        self.name = "JoJoStone"
+        self.color = ()
+        self.is_breakable = False
+        self.solidity = 1000
+
+
 # Блок Грязи
 
 class Dirt(Block):
     def __init__(self, x, y):
-        super().__init__(1, x, y)
+        super().__init__(2, x, y)
 
         self.name = "Dirt"
         self.color = (77, 38, 0)
         self.is_breakable = True
-        self.solidity = 10
+        self.solidity = 25
 
 
 # Блок Камня
 
 class Stone(Block):
     def __init__(self, x, y):
-        super().__init__(2, x, y)
+        super().__init__(3, x, y)
 
         self.name = "Stone"
         self.color = (107, 107, 71)
         self.is_breakable = True
-        self.solidity = 25
+        self.solidity = 35
+
+
+# Блок Меди
+
+class CopperOre(Block):
+    def __init__(self, x, y):
+        super().__init__(4, x, y)
+
+        self.name = "Copper Ore"
+        self.color = (72, 45, 20)
+        self.is_breakable = True
+        self.solidity = 50
+
+
+# Блок Железной Руды
+
+class IronOre(Block):
+    def __init__(self, x, y):
+        super().__init__(5, x, y)
+
+        self.name = "Iron Ore"
+        self.color = (203, 205, 205)
+        self.is_breakable = True
+        self.solidity = 50
