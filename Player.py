0c2412b3df5b3fb7c08370a18465baf067aee2b9
1vanovan <aivanov@fmschool72.ru>
2020-02-20 17:26:59 +0500

    Updated Player: physics Updated Main: player control

--- Player.py
+++ Player.py
@@ -1,5 +1,6 @@
 import pygame
 from Blocks import Block
+import Mobs
 import Main
 from pygame.rect import Rect
 
@@ -7,24 +8,25 @@ from pygame.rect import Rect
 class Player:
     def __init__(self, x, y):
         # координаты по x, y
-        self.x = int()
-        self.y = int()
-
+        self.x = x
+        self.y = y
         self.size_x = Block.size
         self.size_y = 2 * Block.size
-
-        self.vx = int()
-        self.vy = int()
-
+        self.vx = 0
+        self.vy = 0
+        self.sprite = bool()
         self.hp = int()
-        self.jump_height = int()
-        self.is_jump = bool()
-        self.jump_speed = int()
+        self.jump_speed = -10
         self.direction = bool()
+        self.attack = int()
+        self.speed = 10
 
     def move(self):
         self.x += self.vx
         self.y += self.vy
+        if self.vy < 0:
+            self.vy += 10
+
 
     def speed_x(self, x):
         self.vx += x
@@ -32,7 +34,7 @@ class Player:
     def fall(self, y):
         self.vy -= y
 
-    def jump(self, y):
+    def jump(self):
         if self.vy == 0:
             self.vy += self.jump_speed
 
@@ -40,7 +42,15 @@ class Player:
         return self.x / Block.size, self.y / Block.size
 
     def collide(self):
-        b_x, b_y = self.block_now()
-        if (((self.x + self.size_x + 1) % Block.size == 0 and (Main.world[b_x + 1][b_y + 1].solid != -1) or
-            (self.x + 1) % Block.size == 0 and (Main.world.world[b_x + 1][b_y].solid != -1)) and self.x > 0):
-            self.x = 0
+        pass
+
+    def show(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, self.size_x, self.size_y))
+
+    def damage(self, x):
+        self.hp -= x
+
+    def is_live(self):
+        if self.hp > 0:
+            return True
+        return False
